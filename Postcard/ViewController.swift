e790a44c78939dbb83a99dbedf95297ea761806c
Nocebos <Jannis.Schmidt@gmx.de>
2014-09-24 19:53:09 +0200

    first rev of project

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Jannis Schmidt on 11.09.14.
+//  Copyright (c) 2014 Thinking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var massageLabel: UILabel!
+    
+    @IBOutlet var enterNameTextField: UITextField!
+    
+    @IBOutlet var enterAMassageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailButtonPressed(sender: UIButton)
+    {
+        //Code will evaluate when we press the button
+        massageLabel.hidden = false
+        massageLabel.text = enterAMassageTextField.text
+        enterAMassageTextField.text = ""
+        enterAMassageTextField.resignFirstResponder()
+        massageLabel.textColor = UIColor.redColor()
+        mailButton.setTitle("Mail sent", forState: UIControlState.Normal)
+    }
+
+}
+
